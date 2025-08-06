125ab20a83cbf958b9837445ec2543b6da341953
ravishah8176 <ravishah8176@gmail.com>
2025-08-06 22:04:54 +0530

    [CICLOZA-22] - created sample react working app

---
+++ Pulse/src/components/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Pulse</h1>
+      </div>
+    </>
+  )
+}
---
+++ Pulse/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Vite configuration
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // Port for the development server
+  },
+})
